68b7eb605a9935ce1e5f8ee5c4f74072ec626daa
Jon Sadowski <jwsadowski@gmail.com>
2022-09-26 15:30:49 -0500

    new commits

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var timerEl = document.queryselector(".time");
 var timeLeft = doucment.queryselector(".timeleft");
 var mainEl = document.querySelector(".main");
 var textAreaEl = document.querySelector("#textarea");
-var clearEl = 
+
+var questionList = ["Commonly used data types do not inculde: ", "The condition in an if / else statement is enclosed within ____", "Arrays in Javascript can be used to store", "String values must be enclosed within ____ when being assigned to variables", "A very usedful tool used in web development and debugging for printing content to the debugger"]
 
 var secondsLeft = 75;
+
+// call a countdown function that starts at 75 seconds
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -15,5 +18,31 @@ function setTime() {
             clearInterval(timerInterval);
             sendMessage();
         }
+    }, 1000);
+}
+
+// 10 seconds are subtracted from the countdown when user selects the incorrect answer. They're still directed to the next section though.
+function guessedAnswer() {
+    if (questionList[i].correct) {
+        score++
+    } else {
+        timeLeft -= 10;
+        time.textContent = timeLeft;
     }
+
+    if (questionlist[i]) {
+        score = timeLeft;
+        GameOver()
+    } else {
+        renderQuestion()
+    }
+}
+
+// states the high score
+function highScore() {
+    h3EL.innerText = ("High Scores"); 
+}
+// game over screen
+function highScore() {
+    clearInterval(countdown);
 }
